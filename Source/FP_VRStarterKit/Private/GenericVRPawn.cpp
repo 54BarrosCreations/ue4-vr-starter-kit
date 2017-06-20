1ef2f6bfef1a43d03e4f0fa9371beb1693f8427f
Jake Flynn <flynn_jacob@bah.com>
2017-06-20 14:28:06 -0400

    create pawn base class w/ trackable motion controller scene components

---
+++ Source/FP_VRStarterKit/Private/GenericVRPawn.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GenericVRPawn.h"
+
+// Sets default values
+AGenericVRPawn::AGenericVRPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	//Set up default component roots
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Default Scene Root"));
+	RootComponent = DefaultSceneRoot;
+	VROrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VR Origin"));
+	VROrigin->SetupAttachment(DefaultSceneRoot);
+	DefaultCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Default Camera"));
+	DefaultCamera->SetupAttachment(VROrigin);
+	LeftControllerRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Left Controller"));
+	LeftControllerRoot->SetupAttachment(VROrigin);
+	RightControllerRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Right Controller"));
+	RightControllerRoot->SetupAttachment(VROrigin);
+}
+
+// Called when the game starts or when spawned
+void AGenericVRPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGenericVRPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UpdateMotionControllerPositions();
+}
+
+// Called to bind functionality to input
+void AGenericVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AGenericVRPawn::GetMotionControllerIndices()
+{
+	TArray<int32> ControllerIndices;
+	USteamVRFunctionLibrary::GetValidTrackedDeviceIds(ESteamVRTrackedDeviceType::Controller, ControllerIndices);
+	if (ControllerIndices.IsValidIndex(0)) {
+		
+	}
+}
+
+void AGenericVRPawn::UpdateMotionControllerPositions()
+{
+	if (!LeftControllerRoot || !RightControllerRoot) return;
+
+	//Left Controller
+	FVector LPos;
+	FRotator LRot;
+	USteamVRFunctionLibrary::GetTrackedDevicePositionAndOrientation(1, LPos, LRot);
+	LeftControllerRoot->SetRelativeTransform(FTransform(LRot, LPos, FVector(1, 1, 1)));
+
+	//Right Controller
+	FVector RPos;
+	FRotator RRot;
+	USteamVRFunctionLibrary::GetTrackedDevicePositionAndOrientation(2, RPos, RRot);
+	RightControllerRoot->SetRelativeTransform(FTransform(RRot, RPos, FVector(1, 1, 1)));
+}
+
---
+++ Source/FP_VRStarterKit/Public/GenericVRPawn.h
@@ -0,0 +1,108 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "SteamVRFunctionLibrary.h"
+#include "Runtime/Engine/Classes/Camera/CameraComponent.h"
+#include "HeadMountedDisplay.h"
+#include "GameFramework/Pawn.h"
+#include "GenericVRPawn.generated.h"
+
+UCLASS()
+class FP_VRSTARTERKIT_API AGenericVRPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGenericVRPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//~~Pawn Options~~
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers")
+	//If true, will allow the motion controllers to tick. Select false to use gamepad.
+	bool bUseMotionControllers = true;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers")
+	//If true, will enable laser pointer input
+	bool bUseLaserPointer = true;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers|Controller Options", meta = (EditCondition = "bUseMotionControllers"))
+	//Controller Index to assign to the Left Controller Object, if Applicable
+	int32 LeftControllerIndex = 1;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers|Controller Options", meta = (EditCondition = "bUseMotionControllers"))
+	//Controller Index to assign to the Right Controller Object, if Applicable
+	int32 RightControllerIndex = 2;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers|Controller Options", meta = (EditCondition = "bUseMotionControllers"))
+	//If true, Left Controller will be enabled.
+	bool bUseLeftController = true;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers|Controller Options", meta = (EditCondition = "bUseMotionControllers"))
+	//If true, Right Controller will be enabled.
+	bool bUseRightController = true;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Pawn|Motion Controllers|Experimental")
+	//Experimental: Searches for motion controller indices, in case they aren't set to 1 and 2
+	bool bValidateControllerIndices = false;
+
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//~~Inherited Components~~ 
+	
+	UPROPERTY(VisibleDefaultsOnly)
+	//Default root for the pawn
+	USceneComponent* DefaultSceneRoot = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Root Component for Default Camera and Pawn Mesh
+	USceneComponent* VROrigin = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Camera Component
+	UCameraComponent* DefaultCamera = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Root Component for Left Motion Controller Mesh
+	USceneComponent* LeftControllerRoot = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Root Component for Right Motion Controller Mesh
+	USceneComponent* RightControllerRoot = nullptr;
+
+private:
+
+	//Controller Indices
+	int32 mLeftControllerIndex = 1;
+	int32 mRightControllerIndex = 2;
+
+	//Get Motion Controller Indices, in case they are not set to 1 and 2
+	void GetMotionControllerIndices();
+
+	//Update postion of motion controllers
+	void UpdateMotionControllerPositions();
+
+
+
+
+
+
+
+	
+	
+};
