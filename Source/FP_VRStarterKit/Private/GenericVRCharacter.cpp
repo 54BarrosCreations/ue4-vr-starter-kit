4a7b6b428cc5e2fcd1f7a13bca33ea1c492245b4
Jake Flynn <flynn_jacob@bah.com>
2017-07-25 14:35:29 -0400

    Add Motion Controllers to VR Character.

--- Source/FP_VRStarterKit/Private/GenericVRCharacter.cpp
+++ Source/FP_VRStarterKit/Private/GenericVRCharacter.cpp
@@ -9,10 +9,30 @@ AGenericVRCharacter::AGenericVRCharacter(const FObjectInitializer& ObjectInitial
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	VRCameraOrigin = CreateDefaultSubobject<UCameraComponent>(TEXT("VR Camera Origin"));
-	VRCameraOrigin->SetupAttachment(RootComponent);
-	
+	VRSceneRoot = CreateDefaultSubobject<USceneComponent>(FName("VR Scene Root"));
+	RootComponent = VRSceneRoot;
+	VROrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VR Origin"));
+	VROrigin->SetupAttachment(VRSceneRoot);
+	VRCameraOrigin = CreateDefaultSubobject<UCameraComponent>(FName("VR Camera Origin"));
+	VRCameraOrigin->SetupAttachment(VROrigin);
+	GetCapsuleComponent()->SetupAttachment(VRCameraOrigin);
 
+	//Left Motion Controller
+	LeftMotionControllerRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Left Controller"));
+	LeftMotionControllerRoot->SetupAttachment(VROrigin);
+	LeftMotionControllerRoot->SetIsReplicated(true);
+	PS_LeftControllerBeam = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Left Controller Beam"));
+	PS_LeftControllerBeam->SetupAttachment(LeftMotionControllerRoot);
+
+	//Right Motion Controller
+	RightMotionControllerRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Right Controller"));
+	RightMotionControllerRoot->SetupAttachment(VROrigin);
+	RightMotionControllerRoot->SetIsReplicated(true);
+	PS_RightControllerBeam = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Right Controller Beam"));
+	PS_RightControllerBeam->SetupAttachment(RightMotionControllerRoot);
+
+	//InteractionComponent
+	InteractionComponent = CreateDefaultSubobject<UVRInteractionComponent>(TEXT("Interaction Component"));
 }
 
 // Called when the game starts or when spawned
@@ -26,7 +46,8 @@ void AGenericVRCharacter::BeginPlay()
 void AGenericVRCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if (IsLocallyControlled())	UpdateMotionControllerPositions();
+	if (bUseLaserInteraction)	UpdateLaser();
 }
 
 // Called to bind functionality to input
@@ -34,5 +55,63 @@ void AGenericVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInput
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AGenericVRCharacter::SetInitialActiveController()
+{
+	if (!InteractionComponent) return;
+	if (InteractionComponent->TraceForUI(LeftMotionControllerRoot)) {
+		InteractionComponent->SetRightLaserActive(false);
+		InteractionComponent->ControllerFirstTimeActive = false;
+	}
+	else if (InteractionComponent->TraceForUI(RightMotionControllerRoot)) {
+		InteractionComponent->SetRightLaserActive(true);
+		InteractionComponent->ControllerFirstTimeActive = false;
+	}
+}
+
+void AGenericVRCharacter::UpdateMotionControllerPositions()
+{
+	//Left Controller
+	FVector LPos;
+	FRotator LRot;
+	USteamVRFunctionLibrary::GetHandPositionAndOrientation(0, EControllerHand::Left, LPos, LRot);
+	LeftMotionControllerRoot->SetRelativeTransform(FTransform(LRot, LPos, FVector(1, 1, 1)));
+
+	//Right Controller
+	FVector RPos;
+	FRotator RRot;
+	USteamVRFunctionLibrary::GetHandPositionAndOrientation(0, EControllerHand::Right, RPos, RRot);
+	RightMotionControllerRoot->SetRelativeTransform(FTransform(RRot, RPos, FVector(1, 1, 1)));
+}
+
+void AGenericVRCharacter::UpdateLaser()
+{
+	if (InteractionComponent->ControllerFirstTimeActive) SetInitialActiveController();
+	else {
+		FHitResult HitResult;
+		if (InteractionComponent->RightControllerActive) {
+			if (InteractionComponent->TraceForUI(RightMotionControllerRoot, HitResult)) {
+				PS_RightControllerBeam->SetBeamSourcePoint(0, RightMotionControllerRoot->GetComponentLocation(), 0);
+				PS_RightControllerBeam->SetBeamEndPoint(0, InteractionComponent->GetBeamEndPoint(RightMotionControllerRoot, HitResult));
+				if (PS_LeftControllerBeam->bVisible) PS_LeftControllerBeam->SetVisibility(false);
+				if (!PS_RightControllerBeam->bVisible) PS_RightControllerBeam->SetVisibility(true);
+			}
+			else {
+				if (PS_RightControllerBeam->bVisible) PS_RightControllerBeam->SetVisibility(false);
+			}
+		}
+		else if (!InteractionComponent->RightControllerActive) {
+			if (InteractionComponent->TraceForUI(LeftMotionControllerRoot, HitResult)) {
+				PS_LeftControllerBeam->SetBeamSourcePoint(0, LeftMotionControllerRoot->GetComponentLocation(), 0);
+				PS_LeftControllerBeam->SetBeamEndPoint(0, InteractionComponent->GetBeamEndPoint(LeftMotionControllerRoot, HitResult));
+				if (PS_RightControllerBeam->bVisible) PS_RightControllerBeam->SetVisibility(false);
+				if (!PS_LeftControllerBeam->bVisible) PS_LeftControllerBeam->SetVisibility(true);
+			}
+			else {
+				if (PS_LeftControllerBeam->bVisible) PS_LeftControllerBeam->SetVisibility(false);
+			}
+		}
+	}
 }
 
--- Source/FP_VRStarterKit/Public/GenericVRCharacter.h
+++ Source/FP_VRStarterKit/Public/GenericVRCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine.h"
+#include "MotionControllerComponent.h"
 #include "VRInteractionComponent.h"
 #include "SteamVRFunctionLibrary.h"
 #include "Runtime/Engine/Classes/Camera/CameraComponent.h"
@@ -33,11 +34,51 @@ public:
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//~~VR Character Options~~
+	UPROPERTY(EditAnywhere, Category = "Generic VR Character|Motion Controllers")
+	bool bUseMotionControllers = true;
+
+	UPROPERTY(EditAnywhere, Category = "Generic VR Character|Motion Controllers")
+	bool bUseLaserInteraction = true;
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//~~Inherited Components~~ 
 	UPROPERTY(VisibleDefaultsOnly)
 	//Camera Component Root, parent mesh object to this
+	USceneComponent* VRSceneRoot = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Camera Component Root, parent mesh object to this
+	USceneComponent* VROrigin = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	//Camera Component Root, parent mesh object to this
 	UCameraComponent* VRCameraOrigin = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	USceneComponent* LeftMotionControllerRoot = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	USceneComponent* RightMotionControllerRoot = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite)
+	UVRInteractionComponent* InteractionComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	UParticleSystemComponent* PS_LeftControllerBeam = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	UParticleSystemComponent* PS_RightControllerBeam = nullptr;
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//~~Functions~~ 
+
+	UFUNCTION()
+	void SetInitialActiveController();
+
+	UFUNCTION()
+	void UpdateMotionControllerPositions();
+
+	UFUNCTION()
+	void UpdateLaser();
 	
 };
